29abf7c09d35c176b0ccf7202f2252afb83c9e4e
Nas <nassersanou23@gmail.com>
2025-02-06 00:34:25 +0100

    Ads promise to followers

--- app/dashboard/[username]/followers/page.tsx
+++ app/dashboard/[username]/followers/page.tsx
@@ -1,17 +1,33 @@
 import FollowersModal from "@/components/FollowersModal";
 import { fetchProfile } from "@/lib/data";
 
-async function FollowersPage({
-  params: { username },
-}: {
-  params: {
-    username: string;
-  };
-}) {
+export default async function fetchPostById({params} :{ params:Promise <{username: string }>}){
+  const { username } = await params;
+
   const profile = await fetchProfile(username);
   const followers = profile?.followedBy;
 
   return <FollowersModal followers={followers} username={username} />;
 }
 
-export default FollowersPage;
+
+
+
+
+// import FollowersModal from "@/components/FollowersModal";
+// import { fetchProfile } from "@/lib/data";
+
+// async function FollowersPage({
+//   params: { username },
+// }: {
+//   params: {
+//     username: string;
+//   };
+// }) {
+//   const profile = await fetchProfile(username);
+//   const followers = profile?.followedBy;
+
+//   return <FollowersModal followers={followers} username={username} />;
+// }
+
+// export default FollowersPage;
