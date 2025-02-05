6db641e86cc06dec45da799a6600298bf9f5984b
Nas <nassersanou23@gmail.com>
2025-02-06 00:20:39 +0100

    Adss use client to DashboardLayout  && changes proptypes in ProfileLayout

--- app/dashboard/(.)p/[id]/page.tsx
+++ app/dashboard/(.)p/[id]/page.tsx
@@ -2,20 +2,38 @@ import PostView from "@/components/PostView";
 import { fetchPostById } from "@/lib/data";
 import { notFound } from "next/navigation";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
+// type Props = {
+//   params: {
+//     id: string;
+//   };
+// };
 
-async function PostModal({ params: { id } }: Props) {
-  const post = await fetchPostById(id);
+
+export default async function PostModal({params} :{ params:Promise <{postId: string }>}){
+  const { postId } = await params;
+  const post = await fetchPostById(postId);
 
   if (!post) {
     notFound();
   }
 
-  return <PostView id={id} post={post} />;
+  //  Array.isArray(params.id) ? params.id[0] : params.id || ""; // Ensure id is a string
+
+  return (
+    <div>
+     <PostView id={postId} post={post} />;
+    </div>
+  );
 }
 
-export default PostModal;
+// async function PostModal({ params: { id } }: Props) {
+//   const post = await fetchPostById(id);
+
+//   if (!post) {
+//     notFound();
+//   }
+
+//   return <PostView id={id} post={post} />;
+// }
+
+// export default PostModal;
