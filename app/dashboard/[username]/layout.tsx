4ea2cc8fcb1858654aeca91af46bbac8307378d5
Nas <nassersanou23@gmail.com>
2025-02-05 23:17:47 +0100

    Adss use client to DashboardLayout  && changes proptypes in ProfileLayout

--- app/dashboard/[username]/layout.tsx
+++ app/dashboard/[username]/layout.tsx
@@ -11,31 +11,59 @@ import type { Metadata, ResolvingMetadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
+// ... other imports
+
 type Props = {
-  params: {
-    username: string;
-  };
+  params: { username: string };
   children: React.ReactNode;
 };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const username = params.username;
-
-  const profile = await fetchProfile(username);
+  const profile = await fetchProfile(username); // Fetch profile data
 
   return {
-    title: `${profile?.name} (@${profile?.username})`,
+    title: `${profile?.name} (@${profile?.username})` || "Profile" // Handle null profile
   };
 }
 
+
+// type Props = {
+//   params: {
+//     username: string;
+//   };
+//   children: React.ReactNode;
+// };
+
+// export async function generateMetadata(
+//   { params }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const username = params.username;
+
+//   const profile = await fetchProfile(username);
+
+//   return {
+//     title: `${profile?.name} (@${profile?.username})`,
+//   };
+// }
+
+// async function ProfileLayout({ children, params: { username } }: Props) {
+//   const profile = await fetchProfile(username);
+//   const session = await auth();
+//   const isCurrentUser = session?.user.id === profile?.id;
+  //   the followerId here is the id of the user who is following the profile
+  // const isFollowing = profile?.followedBy.some(
+  //   (user) => user.followerId === session?.user.id
+  // );
+
+  // if (!profile) {
+  //   notFound();
+  // }
 async function ProfileLayout({ children, params: { username } }: Props) {
-  const profile = await fetchProfile(username);
+  const profile = await fetchProfile(username); // Fetch profile data (only once)
   const session = await auth();
   const isCurrentUser = session?.user.id === profile?.id;
-  //   the followerId here is the id of the user who is following the profile
   const isFollowing = profile?.followedBy.some(
     (user) => user.followerId === session?.user.id
   );
@@ -43,6 +71,7 @@ async function ProfileLayout({ children, params: { username } }: Props) {
   if (!profile) {
     notFound();
   }
+
   return (
     <>
       <ProfileHeader username={profile.username} />
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import SideNav from "@/components/SideNav";
 
 export default function DashboardLayout({
