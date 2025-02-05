236af3b7a7c25cda828e5394ff0296df630367fd
Nas <nassersanou23@gmail.com>
2025-02-05 22:06:50 +0100

    Add Next.js dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-// import { redirect } from "next/navigation";
-
-// async function Page() {
-//   redirect("/dashboard");
-// }
-
-// export default Page;
-
 
 
 import { redirect } from "next/navigation";
